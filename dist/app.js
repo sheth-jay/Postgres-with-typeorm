470dad0db64e0ee4028d8cf873d4000b08e83e33
sheth-jay <jay.sheth@simformsolutions.com>
2023-12-20 18:04:26 +0530

    basic typeorm setup with Postgres

---
+++ dist/app.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.use(express_1.default.json());
+const port = 3000;
+app.get('/', function (req, res) {
+    res.send('Hello');
+});
+app.listen(port, () => {
+    console.log('Applications is running');
+});
---
+++ src/app.ts
@@ -0,0 +1,86 @@
+import "reflect-metadata";
+import express from "express";
+import { DataSource } from "typeorm";
+import { User } from "./entities/User";
+import { Profile } from "./entities/Profile";
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+
+app.get("/", async function(req, res) {
+  // const { userId } = req.body;
+
+  const userRepo = AppDataSource.getRepository(User);
+  const profileRepo = AppDataSource.getRepository(Profile);
+  //find all the user records
+  const allRecords = await userRepo.find();
+
+  const userFound = await userRepo.findOne({ where: {id: 1 }});
+
+  if(userFound) {
+    userFound.email = "email@gmail.com";
+    userFound.firstName = "abc2";
+    userFound.lastName = "test"
+    userFound.profile.gender = "female";
+    userFound.profile.photo = "new photo";
+
+    const updateRecord = await userRepo.save(userFound);
+    res.json(updateRecord);
+  } else {
+    res.send("Record does not exist");
+  }
+
+  // delete a user
+  // await userRepo.delete(userId);
+
+  // add a new user
+  // let profile: Profile = new Profile();
+
+  // profile.gender = "male";
+  // profile.photo = "this is photo";
+
+  // let user: User = new User();
+  // user.email = "jay1@gmail.com";
+  // user.firstName = "jay1";
+  // user.lastName = "sheth";
+  // user.profile = profile;
+
+  // const userInserted = userRepo.save(user);
+
+  // update a user
+
+  // const updatedUser = await userRepo.update(userId, {
+  //   firstName: "jay2",
+  //   lastName: "sheth",
+  // });
+
+  // filter user
+
+  // const user = await userRepo.findOne({ where: { id: userId }});
+
+  res.send();
+});
+
+const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "Lazovw$6.",
+  database: "typeorm",
+  entities: ["src/entities/*{.ts, .js}"],
+  synchronize: true,
+  logging: true,
+});
+
+AppDataSource.initialize()
+  .then(() => {
+    console.log("Database connected successfully");
+    app.listen(port, () => {
+      console.log(`Application is running on ${port}`);
+    });
+  })
+  .catch(err => console.log("Error while connecting to database", err));
